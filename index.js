5bb467832fc598b979f33df7d3fc50e68d868e64
udoye adaobi <adaobimaryann150@gmail.com>
2025-12-15 19:33:17 +0100

    additional changes to my initial commit

--- index.js
+++ index.js
@@ -1,41 +1,125 @@
 // name prompt
 
-let x = prompt("Enter Full Name ");
 
-while (!x || x.split(" ").length < 2  ) {
-    x = prompt(" Please Enter Full Name ");
+function nameValidation(){
+let x = prompt("Enter Full Name: Firstname , Lastname ");
+
+if(x===null){
+    return;
 }
- alert("Name  entered correctly");
+
+
+while (x.split(" ").length < 2){
+    x = prompt(" please Enter Full Name ");
+    if(x===null){
+    return;
+}
+
+}
+alert("Name entered correctly!")
+
+
+}
+
+nameValidation()
+
+
+
+
 
 // email prompt
+
+function emailValidation() {
 
 let email = prompt("Enter Email Address");
 let emailPattern =/^\w+@gmail\.com$/;
 
-while (!emailPattern.test(email) ){
- email = prompt("Enter Correct Email ");
+ if(email === null){
+        return;
+    }
+
+    while (!emailPattern.test(email)){
+     email = prompt("Please Enter Correct Email ");
+     if(email === null){
+        return;
+    }
+   } 
+   alert(" email entered correctly ");
+    
+         
 }
- alert("email entered correctly");
+  emailValidation()  
 
 //password
+let password;
 
-let password = prompt("Enter Password");
+function passwordValidation() {
+
+ password = prompt("Enter Password");
 let passwordPattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()+_]).{8,}$/;
+
+if(password === null){
+    return;
+}
 
 while (!passwordPattern.test(password)) {
   password = prompt( "Enter correct password");
-  
+
+  if(password=== null){
+    return;
+}
 }
 alert("Password entered correctly!");
 
+}
+
+// confirm password 
+
+function passwordConfirmation() {
+let  confirmpassword = prompt("Confirm Password");
+
+if ( confirmpassword=== null){
+    return;
+}
+while( confirmpassword !== password ){
+     confirmpassword= prompt("ReConfirm Password");
+if ( confirmpassword === null){
+    return;
+}
+}
+alert("password confirmed!");
+
+
+}
+passwordValidation()
+passwordConfirmation() 
+ 
+
 // age
 
-let input= prompt("Enter your age");
-let age =Number( input);
+ function ageValidation(){
 
-if (age < 18 ){
-    alert("child");
-    
-}else{
-    alert("adult");
+let input= prompt("Enter your age");
+ if(input===null){
+     return;
 }
+let age = Number(input);
+if (isNaN(age)){
+    alert("please enter a number");
+    return;
+}
+
+ if (age < 18 ) {
+
+    alert("sorry you must be an adult");
+  return;
+}
+alert("adult");
+    
+ }
+
+
+
+    
+
+ageValidation()
